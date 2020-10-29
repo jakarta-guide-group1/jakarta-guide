c9d0b72342eeadcf3e42261eeae8616047bde828
enricoanto <enricoanto@yahoo.com>
2020-10-30 01:48:23 +0700

    dev-server-api add api zomato

--- server/app.js
+++ server/app.js
@@ -2,13 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3000
-
 const router = require('./routers/index.js')
-const errorHandler = require('./middleware/errorHandler')
+const errorHandler = require('./middlewares/errorHandler')
 
-app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use('/', router)
 app.use(errorHandler)
 
--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const weatherRouter = require('./weatherRouter')
-
-router.use(weatherRouter)
-
-module.exports = router
