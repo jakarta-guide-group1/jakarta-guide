5582f33a0e873b3d53fbf5bf66fedaa3a07427ef
Nikolas Stefano <nz.stefano@gmail.com>
2020-10-30 04:18:32 +0700

    Google Sign In and fix some UI Logic

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
-// const userRouter = require("./userRouter");
+const userRouter = require("./userRouter");
 // const travelPlanRouter = require("./travelPlanRouter");
 // const hotelRouter = require("./hotelRoter");
-// const weatherRouter = require('./weatherRouter');
+const weatherRouter = require("./weatherRouter");
 const Restaurant = require("../controllers/restaurantController");
 
-// router.use(weatherRouter)
-// router.use(userRouter);
-router.get('/restaurant', Restaurant.fetchRestaurants)
+router.use(weatherRouter);
+router.use(userRouter);
+router.get("/restaurant", Restaurant.fetchRestaurants);
 
 module.exports = router;
